63335899b3adc14c2bb0fba81971554b477db42d
Lililiuliu <391278659@qq.com>
2019-07-22 13:49:47 +0800

    重构代码+实现了跳转

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,4 @@
-//测试github
 var app = getApp();
-
-
 
 Page({
 
@@ -9,7 +6,7 @@ Page({
     winHeight: "", //窗口高度
     currentTab: 0, //预设当前项的值
     scrollLeft: 0, //tab标题的滚动条位置
-    newsList: [{ //假数据
+    newsList: [{ 
       img: "",
       title: "",
       soure: "",
@@ -41,7 +38,7 @@ Page({
 
   // 点击标题切换
   swichNav: function(e) {
-    var cur = e.target.dataset.current;
+    let cur = e.target.dataset.current;
     if (this.data.currentTaB == cur) {
       return false;
     } else {
@@ -53,17 +50,11 @@ Page({
   },
 
   onLoad: function() {
-    var that = this;
-    
+    let that = this;
     // 获取bar高度
     wx.getSystemInfo({
       success: function(res) {
-        let navBarHeight = 0
-        if (res.model.indexOf('iPhone') !== -1) {
-          navBarHeight = 44
-        } else {
-          navBarHeight = 48
-        }
+        let navBarHeight = res.model.indexOf('iPhone') !== -1 ? 44 : 48
         that.setData({
           statusBarHeight: res.statusBarHeight,
           navBarHeight: navBarHeight
@@ -80,10 +71,10 @@ Page({
     //  高度自适应
     wx.getSystemInfo({
       success: function(res) {
-        var clientHeight = res.windowHeight,
+        const clientHeight = res.windowHeight,
           clientWidth = res.windowWidth,
           rpxR = 750 / clientWidth;
-        var calc = clientHeight * rpxR - 180;
+        const calc = clientHeight * rpxR - 180;
         console.log(calc)
         that.setData({
           winHeight: calc
@@ -92,7 +83,7 @@ Page({
     });
 
     // 获取scrollview的margin-top
-    var query = wx.createSelectorQuery();
+    const query = wx.createSelectorQuery();
     query.select('.tab-h').boundingClientRect()
     query.exec(function(res) {
       that.setData({
@@ -118,18 +109,24 @@ Page({
     let source = e.data.result[0].source
     let img = e.data.result[0].firstImage
     let id = e.data.result[0].id
+
+    if (img.slice(0,4) !== "http") {
+      img = "http://"+img
+    } else {
+      img = img
+    }
     
-    let topTitle = "topNews.title"
-    let topDate = "topNews.date"
-    let topSource = "topNews.source"
-    let topImg = "topNews.img"
-    let topId = "topNews.id"
+    const topTitle = "topNews.title"
+    const topDate = "topNews.date"
+    const topSource = "topNews.source"
+    const topImg = "topNews.img"
+    const topId = "topNews.id"
 
     this.setData({
       [topTitle]:title,
       [topDate]: date.slice(11,16), 
       [topSource]: source,
-      [topImg]: "http://"+img,
+      [topImg]: img,
       [id]: id
     })
 
@@ -141,7 +138,16 @@ Page({
     let newsList = e.data.result
     let list = []
 
+
     for (let i = 1; i < newsList.length; i += 1) {
+      
+      
+      if (newsList[i].img !== "http") {
+        newsList[i].img = "http://" + newsList[i].img
+      } else {
+        newsList[i].img = newsList[i].img
+      }
+
       list.push({
         id: newsList[i].id,
         title: newsList[i].title,
@@ -186,5 +192,4 @@ Page({
     
   }
 
-  // footerTap: app.footerTap
 })
